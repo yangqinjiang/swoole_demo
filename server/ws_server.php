f659f91286f72e5e979878cd1960d68a0776a202
yangqinjiang vm <yangqinjiang@qq.com>
2019-07-05 11:51:26 +0800

    ws的优化,task的使用

--- server/ws_server.php
+++ server/ws_server.php
@@ -1,6 +1,12 @@
 <?php
 $server = new swoole_websocket_server("0.0.0.0", 8812);
-
+//静态文件
+// curl http://127.0.0.1:8812/hello.html
+//当程序找到静态文件时，则输出它,否则向下执行
+$server->set([
+        'enable_static_handler'=>true,
+        'document_root'=>"/home/root/study/code/static"
+]);
 $server->on('open', function($server, $req) {
     echo "connection open: {$req->fd}\n";
 });
@@ -13,5 +19,5 @@ $server->on('message', function($server, $frame) {
 $server->on('close', function($server, $fd) {
     echo "connection close: {$fd}\n";
 });
-echo "open http://127.0.0.1:8811/ws_client.html";
+echo "open http://127.0.0.1:8812/ws_client.html\n";
 $server->start();
