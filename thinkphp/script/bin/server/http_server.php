371428a3a0aecbfc7769be2e8167c345a3091004
yangqinjiang <531553280@qq.com>
2019-07-06 22:09:27 +0800

    整合thinkphp 5

--- thinkphp/script/bin/server/http_server.php
+++ thinkphp/script/bin/server/http_server.php
@@ -10,7 +10,7 @@ $http = new swoole_http_server("0.0.0.0", 8811);
 $http->set(
     [
         'enable_static_handler' => true,
-        'document_root' => "/home/work/hdtocs/swoole_mooc/thinkphp/public/static",
+        'document_root' => __DIR__."/../../../public/static",//静态文件夹,
         'worker_num' => 5,
     ]
 );
@@ -18,7 +18,7 @@ $http->on('WorkerStart', function(swoole_server $server,  $worker_id) {
     // 定义应用目录
     define('APP_PATH', __DIR__ . '/../application/');
     // 加载框架里面的文件
-    require __DIR__ . '/../thinkphp/base.php';
+    //require __DIR__ . '/../thinkphp/base.php';
     //require __DIR__ . '/../thinkphp/start.php';
 });
 $http->on('request', function($request, $response) use($http){
