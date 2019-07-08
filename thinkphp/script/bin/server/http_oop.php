582a199cfa810c69547af709a8b897a137f8f1d4
yangqinjiang <531553280@qq.com>
2019-07-08 11:25:23 +0800

    将swoole_http_server编写成对象

--- thinkphp/script/bin/server/http_oop.php
+++ thinkphp/script/bin/server/http_oop.php
@@ -1,9 +1,6 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: baidu
- * Date: 18/3/27
- * Time: 上午12:50
+ * 将swoole_http_server编写成对象
  */
 class Http {
     CONST HOST = "0.0.0.0";
@@ -16,7 +13,7 @@ class Http {
         $this->http->set(
             [
                 'enable_static_handler' => true,
-                'document_root' => "/home/work/hdtocs/swoole_mooc/thinkphp/public/static",
+                'document_root' => __DIR__."/../../../public/static",//静态文件夹,
                 'worker_num' => 4,
                 'task_worker_num' => 4,
             ]
@@ -31,16 +28,16 @@ class Http {
         $this->http->start();
     }
 
+    //分别有 worker_num个进程启动下面的代码
     /**
      * @param $server
      * @param $worker_id
      */
     public function onWorkerStart($server,  $worker_id) {
-        // 定义应用目录
-        define('APP_PATH', __DIR__ . '/../application/');
-        // 加载框架里面的文件
-        //require __DIR__ . '/../thinkphp/base.php';
-        require __DIR__ . '/../thinkphp/start.php';
+            // 定义应用目录
+            define('APP_PATH', __DIR__ . '/../../../application/');
+            // 加载框架里面的文件
+            require __DIR__ . '/../../../thinkphp/base.php';
     }
 
     /**
@@ -49,36 +46,42 @@ class Http {
      * @param $response
      */
     public function onRequest($request, $response) {
-        $_SERVER  =  [];
+        echo "some one request".PHP_EOL;
+        //还原$_SERVER数据
+        $_SERVER  =  [];//一定要清空,否则下次过来会保持上次请求的数据
         if(isset($request->server)) {
             foreach($request->server as $k => $v) {
                 $_SERVER[strtoupper($k)] = $v;
             }
         }
+        //还原header数据
         if(isset($request->header)) {
             foreach($request->header as $k => $v) {
                 $_SERVER[strtoupper($k)] = $v;
             }
         }
-
-        $_GET = [];
+        //还原$_GET数据
+        $_GET = [];//一定要清空,否则下次过来会保持上次请求的数据
         if(isset($request->get)) {
             foreach($request->get as $k => $v) {
                 $_GET[$k] = $v;
             }
         }
-        $_POST = [];
+        //还原$_POST数据
+        $_POST = [];//一定要清空,否则下次过来会保持上次请求的数据
         if(isset($request->post)) {
             foreach($request->post as $k => $v) {
                 $_POST[$k] = $v;
             }
         }
 
-        $_POST['http_server'] = $this->http;
+        $_POST['http_server'] = $this->http;//保持http实例
 
         ob_start();
         // 执行应用并响应
+        //捕获异常
         try {
+            //container有命名空间
             think\Container::get('app', [APP_PATH])
                 ->run()
                 ->send();
@@ -86,9 +89,11 @@ class Http {
             // todo
         }
 
-        $res = ob_get_contents();
+        //echo "-action-".request()->action().PHP_EOL;
+        $res = ob_get_contents();//读取缓冲区的内容
         ob_end_clean();
         $response->end($res);
+        //$http->close();//粗暴地关闭链接.可清空变量??但会在终端上报错
     }
 
     /**
@@ -104,13 +109,7 @@ class Http {
 
         $method = $data['method'];
         $flag = $obj->$method($data['data']);
-        /*$obj = new app\common\lib\ali\Sms();
-        try {
-            $response = $obj::sendSms($data['phone'], $data['code']);
-        }catch (\Exception $e) {
-            // todo
-            echo $e->getMessage();
-        }*/
+        
 
         return $flag; // 告诉worker
     }
