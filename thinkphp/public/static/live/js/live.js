15cf164ec962a3c02b2d47c0aa1c0f5b663120cd
yangqinjiang <531553280@qq.com>
2019-07-09 23:42:11 +0800

    测试图片直播数据

--- thinkphp/public/static/live/js/live.js
+++ thinkphp/public/static/live/js/live.js
@@ -6,6 +6,7 @@ websocket.onopen = function (evt) {
 }
 websocket.onmessage = function (evt) {
     console.log("ws-server-return-data:" + evt.data);
+    push(evt.data);
 }
 websocket.onclose = function (evt) {
     console.log("close");
@@ -13,4 +14,23 @@ websocket.onclose = function (evt) {
 
 websocket.onerror = function (evt, e) {
     console.log("error:" + evt.data);
+}
+function push(raw_data) {
+    var data = JSON.parse(raw_data);
+    var html = '<div class="frame">';
+    html += '<h3 class="frame-header">';
+    html += '<i class="icon iconfont icon-shijian"></i>第' + data.type + '节 01：30';
+    html += '</h3>';
+    html += '<div class="frame-item">';
+    html += ' <span class="frame-dot"></span>';
+    html += '<div class="frame-item-author">';
+    if (data.logo) {
+        html += '<img src="' + data.logo + '" width="20px" height="20px" />';
+    }
+    html += data.title + '</div>';
+    html += '<p>' + data.content + '</p>';
+    html += '</div>';
+    html += '</div>';
+    $('#match-result').prepend(html);
+
 }
