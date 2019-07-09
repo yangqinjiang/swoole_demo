e397d22626373adde48fc54f5af01fa718e1a481
yangqinjiang <531553280@qq.com>
2019-07-09 11:34:29 +0800

    swoole 图片文件上传

--- thinkphp/application/admin/controller/Image.php
+++ thinkphp/application/admin/controller/Image.php
@@ -9,13 +9,13 @@ class Image
         $file = request()->file('file');
         $info = $file->move(APP_PATH.'../public/static/upload');
         if($info) {
-            // 成功上传后 获取上传信息
-            // 输出 jpg
-            echo $info->getExtension();
-            // 输出 20160820/42a79759f284b767dfcb2a0197904287.jpg
-            echo $info->getSaveName();
-            // 输出 42a79759f284b767dfcb2a0197904287.jpg
-            echo $info->getFilename(); 
+            // // 成功上传后 获取上传信息
+            // // 输出 jpg
+            // echo $info->getExtension();
+            // // 输出 20160820/42a79759f284b767dfcb2a0197904287.jpg
+            // echo $info->getSaveName();
+            // // 输出 42a79759f284b767dfcb2a0197904287.jpg
+            // echo $info->getFilename(); 
             $data = [
                 'image' => config('live.host')."/upload/".$info->getSaveName(),
             ];
