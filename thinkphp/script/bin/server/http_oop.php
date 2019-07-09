e139a5c53cfd4bc57e450a9ed9dbd315a495a0f3
yangqinjiang <531553280@qq.com>
2019-07-09 12:42:21 +0800

    server重启,清空数据

--- thinkphp/script/bin/server/http_oop.php
+++ thinkphp/script/bin/server/http_oop.php
@@ -9,6 +9,8 @@ class HttpOOP {
 
     public $http = null;
     public function __construct() {
+        //TODO: 判断redis的 websocket的fd 是否存在, 是否清空
+        \app\common\lib\redis\Predis::getInstance()->del(config('redis.live_game_key'));
         $this->http = new swoole_websocket_server(self::HOST, self::PORT);
         $this->http->listen(self::HOST, self::CHART_PORT, SWOOLE_SOCK_TCP);
         $this->http->set(
